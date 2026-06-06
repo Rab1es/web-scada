83a985c533e8ee1579e8be1997dca289e06c5aa0
Dmytro Sheremeta <dimash100500@gmail.com>
2026-06-06 10:39:07 +0300

    update socket connection

--- client/apps/shell/src/App.tsx
+++ client/apps/shell/src/App.tsx
@@ -1,15 +1,21 @@
-import React, { Suspense, useEffect, useState } from "react";
+import React, { Suspense, useEffect, useRef, useState } from "react";
 import { WS_URL } from "./config";
-import type { SensorData } from "@scada/shared-types";
+import {
+  ModbusCoilsMap,
+  SocketEvent,
+  type SensorData,
+} from "@scada/shared-types";
 
 const RemoteSensorPanel = React.lazy(() => import("monitoring/SensorPanel"));
 
 function App() {
   const [isConnected, setIsConnected] = useState(false);
   const [sensorData, setSensorData] = useState<SensorData | null>(null);
+  const socketRef = useRef<WebSocket | null>(null);
 
   useEffect(() => {
     const socket = new WebSocket(WS_URL);
+    socketRef.current = socket;
 
     socket.onopen = () => setIsConnected(true);
     socket.onclose = () => setIsConnected(false);
@@ -24,17 +30,56 @@ function App() {
     };
 
     return () => {
-      socket.onopen = null;
-      socket.onclose = null;
-      socket.onmessage = null;
+      if (socketRef.current) {
+        socket.onopen = null;
+        socket.onclose = null;
+        socket.onmessage = null;
 
-      socket.close();
+        socketRef.current.close();
+        socketRef.current = null;
+      }
     };
   }, []);
+
+  const handleTogglePump = (value: boolean) => {
+    if (socketRef.current && socketRef.current.readyState === WebSocket.OPEN) {
+      const commandMessage = {
+        command: SocketEvent.SET_PUMP,
+        value,
+        address: ModbusCoilsMap.PUMP,
+      };
+
+      socketRef.current.send(JSON.stringify(commandMessage));
+    } else {
+      console.warn("No socket connection");
+    }
+  };
+
+  const handleToggleHeater = (value: boolean) => {
+    if (socketRef.current && socketRef.current.readyState === WebSocket.OPEN) {
+      const commandMessage = {
+        command: SocketEvent.SET_HEATER,
+        value,
+        address: ModbusCoilsMap.HEATER,
+      };
+
+      socketRef.current.send(JSON.stringify(commandMessage));
+    } else {
+      console.warn("No socket connection");
+    }
+  };
   return (
     <div>
-      <span>shell</span>
-      <h3>Виджет из микрофронтенда:</h3>
+      <div style={{ display: "flex", gap: "5px", marginBottom: "10px" }}>
+        <button onClick={() => handleTogglePump(true)}>Turn on pump</button>
+        <button onClick={() => handleTogglePump(false)}>Turn of pump</button>
+      </div>
+      <div style={{ display: "flex", gap: "5px", marginBottom: "10px" }}>
+        <button onClick={() => handleToggleHeater(true)}>Turn on heater</button>
+        <button onClick={() => handleToggleHeater(false)}>
+          Turn of heater
+        </button>
+      </div>
       <Suspense fallback={<span>Loading MF</span>}>
         <RemoteSensorPanel {...sensorData} isConnected={isConnected} />
       </Suspense>
--- packages/shared-types/index.ts
+++ packages/shared-types/index.ts
@@ -3,8 +3,35 @@ export interface SensorData {
   pressure?: number;
   battareyLevel?: number;
   waterLevel?: number;
+  pumpActive?: boolean;
+  heaterActive?: boolean;
+  systemState?: string;
+  pumpSpeed?: number;
 }
 
 export interface SensorPanelProps extends SensorData {
   isConnected: boolean;
 }
+
+export const SocketEvent = {
+  CONNECTION: "connection",
+  DISCONNECT: "disconnect",
+  SET_HEATER: "set-heater",
+  SET_PUMP: "set-pump",
+  SET_SETPOINT: "set-setpoint",
+};
+
+export const ModbusRegisterMap: Record<string, number> = {
+  TEMPERATURE: 0,
+  PRESSURE: 1,
+  WATER_LEVEL: 2,
+  BATTERY_LEVEL: 3,
+  TEMPERATURE_SETPOINT: 4,
+};
+
+export const ModbusCoilsMap: Record<string, number> = {
+  HEATER: 0,
+  PUMP: 1,
+  HAS_GRID_POWER: 2,
+  ALARM_ERROR: 3,
+};
